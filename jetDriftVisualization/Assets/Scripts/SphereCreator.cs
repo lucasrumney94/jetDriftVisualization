7fb3e58c5b6265023480fdebaf037bf00a0acb44
Hardy Whitney <htw6174@gmail.com>
2016-01-15 14:43:01 -0500

    Properly vertex-ordered spheres

---
+++ jetDriftVisualization/Assets/Scripts/SphereCreator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class SphereCreator : MonoBehaviour {
+
+    [Range(0, 5)]
+    public int subdivisions;
+    public float radius;
+    public Mesh mesh;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    private void FillVerticies()
+    {
+
+    }
+}
