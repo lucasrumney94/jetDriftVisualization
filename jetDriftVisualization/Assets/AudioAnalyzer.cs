19ef3d7a6acd34dec9da0bc83af52fcc297b7344
Lucas Rumney <lucasrumney94@gmail.com>
2016-01-15 13:56:16 -0500

    Started actual clean audio implementation

---
+++ jetDriftVisualization/Assets/AudioAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioAnalyzer : MonoBehaviour
+{
+
+    public float RMSValue;
+    public float DbValue;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
