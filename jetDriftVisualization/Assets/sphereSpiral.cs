32c03b73ad1a1f585cd244bbd9e6b7fdbc6df5b2
Lucas Rumney <lucasrumney94@gmail.com>
2016-01-16 19:05:00 -0500

    optimization for Linspace code; 20fps added to project;

--- jetDriftVisualization/Assets/sphereSpiral.cs
+++ jetDriftVisualization/Assets/sphereSpiral.cs
@@ -6,7 +6,7 @@ using System.Linq;
 public class sphereSpiral : MonoBehaviour {
 
     
-    public const int numberOfSpheres = 512;
+    public const int numberOfSpheres = 2048;
 
     public GameObject spiralSphere;
     public bool goldenRatio = true;
@@ -26,11 +26,14 @@ public class sphereSpiral : MonoBehaviour {
 
     private float theta = 0.0f;
     private float r = 0.0f;
-    private float y = 0.0f;
+    private List<float> ys = new List<float>();
     private float radius = 0.0f;
+
+    private float sqrtNumOfSpheres; 
 
     private List<GameObject> spheres = new List<GameObject>();
     private waveGenerator waveGen;
+    private Vector3 tempPos = new Vector3(0.0f,0.0f,0.0f);
 
     // Use this for initialization
     void Start ()
@@ -52,25 +55,31 @@ public class sphereSpiral : MonoBehaviour {
         if (flange)
             seedAngle = Mathf.Sin(2 * Mathf.PI * Time.time * flangeAmount);
 
-
         waveGen = this.gameObject.GetComponent<waveGenerator>();
+        sqrtNumOfSpheres = Mathf.Sqrt(numberOfSpheres);
+
+        ys = libSVM.Numpy.LinSpace(1 - 1 / numberOfSpheres, 1 / numberOfSpheres - 1, numberOfSpheres).ToList<double>().ConvertAll(x => (float)x);
+
         for (int i = 0; i < numberOfSpheres; i++)
         {
             theta = i * seedAngle;
-            r = Mathf.Sqrt(i) / Mathf.Sqrt(numberOfSpheres);
-            y = (float)libSVM.Numpy.LinSpace(1 - 1 / numberOfSpheres, 1/ numberOfSpheres - 1, numberOfSpheres).ElementAt<double>(i);
-            radius = Mathf.Sqrt(1 - y * y);
-            positions[i] = new Vector3(radius*Mathf.Cos(theta),y,radius*Mathf.Sin(theta));
+            r = Mathf.Sqrt(i) / sqrtNumOfSpheres;
+            
+            radius = Mathf.Sqrt(1 - ys[i] * ys[i]);
+            positions[i] = new Vector3(radius*Mathf.Cos(theta),ys[i],radius*Mathf.Sin(theta));
 
             GameObject temp = GameObject.Instantiate(spiralSphere, positions[i], Quaternion.identity) as GameObject;
             temp.transform.parent = this.gameObject.transform;
             spheres.Add(temp);
         }
-	}
+        Debug.Log(ys.Count);
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
+        /*
         if (goldenRatio)
             seedAngle = Mathf.PI * (3 - Mathf.Sqrt(5));
         if (sqrt2)
@@ -85,16 +94,23 @@ public class sphereSpiral : MonoBehaviour {
             seedAngle = Mathf.Exp(1);
         if (other)
             seedAngle = otherNum;
+        */
         if (flange)
             seedAngle = Mathf.Sin(2 * Mathf.PI * Time.time * flangeAmount);
+        
 
         for (int i = 0; i < numberOfSpheres; i++)
         {
             theta = i * seedAngle;
-            r = Mathf.Sqrt(i) / Mathf.Sqrt(numberOfSpheres);
-            y = (float)libSVM.Numpy.LinSpace(1 - 1 / numberOfSpheres, 1 / numberOfSpheres - 1, numberOfSpheres).ElementAt<double>(i);
-            radius = Mathf.Sqrt(1 - y * y);
-            spheres[i].transform.position = new Vector3(radius * Mathf.Cos(theta) * waveGen.Samples[i], y, radius * Mathf.Sin(theta) * waveGen.Samples[i]);
+            r = Mathf.Sqrt(i) / sqrtNumOfSpheres;
+            
+            radius = Mathf.Sqrt(1 - ys[i] * ys[i]);
+            tempPos.x = radius * Mathf.Cos(theta) * waveGen.Samples[i];
+            tempPos.y = ys[i];
+            tempPos.z = radius * Mathf.Sin(theta) * waveGen.Samples[i];
+            spheres[i].transform.position = tempPos;
+
+            //spheres[i].transform.position = new Vector3(radius * Mathf.Cos(theta) * waveGen.Samples[i], y, radius * Mathf.Sin(theta) * waveGen.Samples[i]);
         }
     }
 }
