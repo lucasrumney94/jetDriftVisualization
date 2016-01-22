166aecec72b0a1ab5ff0337c462f64492670c18d
Lucas Rumney <lucasrumney94@gmail.com>
2016-01-21 22:36:45 -0500

    wasted a couple hours

---
+++ jetDriftVisualization/Assets/swarmObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class swarmObject : MonoBehaviour {
+
+    public Vector3 targetGridSquare = new Vector3();
+
+	// Use this for initialization
+	void Awake ()
+    {
+        gameObject.GetComponent<Renderer>().material = new Material(Shader.Find("Unlit/Color"));
+        //gameObject.GetComponent<Renderer>().material.SetFloat("_Mode", 3);
+        //gameObject.GetComponent<Renderer>().material.SetFloat("_Glossiness", 0);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        //gameObject.GetComponent<Renderer>().material.SetFloat("_Mode", 3);
+        //gameObject.GetComponent<Renderer>().material.SetFloat("_Glossiness", 0);
+        this.gameObject.GetComponent<Rigidbody>().AddForce((targetGridSquare - this.gameObject.transform.position)+9*Random.insideUnitSphere);
+	}
+}
