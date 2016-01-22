4cebb7a64537c7334fcb5000ab30297ec5f9b742
Lucas Rumney <lucasrumney94@gmail.com>
2016-01-21 23:01:05 -0500

    added increasemyFreq

---
+++ jetDriftVisualization/Assets/increaseMyFreq.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class increaseMyFreq : MonoBehaviour {
+
+    public float increment = 0.0001f;
+    private waveGenerator waveGen;
+	// Use this for initialization
+	void Start ()
+    {
+        waveGen = gameObject.GetComponent<waveGenerator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        waveGen.freq += increment;
+	}
+}
