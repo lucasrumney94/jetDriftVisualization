3e5585f8fe13cb6e69986b8dd31b2943d11fa227
Lucas Rumney <lucasrumney94@gmail.com>
2016-02-17 00:05:25 -0500

    added roatiton

--- jetDriftVisualization/Assets/swarmObject.cs
+++ jetDriftVisualization/Assets/swarmObject.cs
@@ -17,10 +17,23 @@ public class swarmObject : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate ()
     {
+        //attempts to make alpha color -> see through texture
         //gameObject.GetComponent<Renderer>().material.SetFloat("_Mode", 3);
         //gameObject.GetComponent<Renderer>().material.SetFloat("_Glossiness", 0);
-        this.gameObject.GetComponent<Rigidbody>().AddForce((targetGridSquare - this.gameObject.transform.position)+9*Random.insideUnitSphere);
 
-        this.gameObject.GetComponent<Rigidbody>().AddForce(5*new Vector3(0, 0, Mathf.Sin(Time.time + targetGridSquare.magnitude / 10*Mathf.Sin(Time.time/10))));
+
+
+        //This line makes it random
+        //this.gameObject.GetComponent<Rigidbody>().AddForce((targetGridSquare - this.gameObject.transform.position)+9*Random.insideUnitSphere);
+
+        //This line is precise
+        this.gameObject.GetComponent<Rigidbody>().AddForce((targetGridSquare - this.gameObject.transform.position));
+
+        //this line distorts the thing as a whole with a sin
+        this.gameObject.GetComponent<Rigidbody>().AddForce(5*new Vector3(0, 0, Mathf.Sin(Time.time + targetGridSquare.magnitude / 5*Mathf.Sin(Time.time/10))));
+
+
+        //This line rotates them
+        this.gameObject.GetComponent<Rigidbody>().AddTorque(5 * new Vector3(0, 0, Mathf.Sin(Time.time + targetGridSquare.magnitude / 10 * Mathf.Sin(Time.time))));
     }
 }
