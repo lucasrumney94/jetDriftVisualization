24e0d0fbb8e6d3d168eef9dbb8902caa65684add
Lucas Rumney <lucasrumney94@gmail.com>
2016-02-16 23:40:40 -0500

    updated to look cool

--- jetDriftVisualization/Assets/swarmObject.cs
+++ jetDriftVisualization/Assets/swarmObject.cs
@@ -21,6 +21,6 @@ public class swarmObject : MonoBehaviour {
         //gameObject.GetComponent<Renderer>().material.SetFloat("_Glossiness", 0);
         this.gameObject.GetComponent<Rigidbody>().AddForce((targetGridSquare - this.gameObject.transform.position)+9*Random.insideUnitSphere);
 
-        this.gameObject.GetComponent<Rigidbody>().AddForce(5*new Vector3(0, 0, Mathf.Sin(Time.time + targetGridSquare.magnitude/5)));
+        this.gameObject.GetComponent<Rigidbody>().AddForce(5*new Vector3(0, 0, Mathf.Sin(Time.time + targetGridSquare.magnitude / 10*Mathf.Sin(Time.time/10))));
     }
 }
