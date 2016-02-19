a8d23a647593aec6ad4b6867cc87aea03f4e976b
Hardy Whitney <htw6174@gmail.com>
2016-02-18 21:26:12 -0500

    test for ui positiont stusipdgjadsgsdg

---
+++ jetDriftVisualization/Assets/Scripts/UI/MarkerMover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MarkerMover : MonoBehaviour
+{
+    public RectTransform canvas;
+
+    public RectTransform verticalMarker;
+    public RectTransform horizontalMarker;
+
+    public Text MarkerPositionDisplay;
+
+    void FixedUpdate()
+    {
+        verticalMarker.position += Vector3.right;
+        horizontalMarker.position += Vector3.up;
+        if (verticalMarker.position.x > canvas.rect.width)
+        {
+            verticalMarker.position = Vector3.zero;
+        }
+        if (horizontalMarker.position.y > canvas.rect.height)
+        {
+            horizontalMarker.position = Vector3.zero;
+        }
+        MarkerPositionDisplay.text = verticalMarker.position.x + " X, " + horizontalMarker.position.y + "Y";
+    }
+}
