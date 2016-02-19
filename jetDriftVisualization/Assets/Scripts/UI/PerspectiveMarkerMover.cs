c2bb410f22b4a7af8e927b599b267622adb00014
Hardy Whitney <htw6174@gmail.com>
2016-02-18 22:32:05 -0500

    updates to gridTestScene

---
+++ jetDriftVisualization/Assets/Scripts/UI/PerspectiveMarkerMover.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PerspectiveMarkerMover : MonoBehaviour
+{
+    public float markerSpeed = 1f;
+
+    public float cameraDistance = 50f;
+
+    public Transform verticalMarker;
+    public Transform horizontalMarker;
+
+    public Text markerText;
+
+    private float cameraScreenedgeAngle;
+
+    void Start()
+    {
+        cameraScreenedgeAngle = Camera.main.fieldOfView / 2f;
+        verticalMarker.position = new Vector3(-Mathf.Tan(cameraScreenedgeAngle * Mathf.Deg2Rad) * cameraDistance, 0f, cameraDistance);
+        horizontalMarker.position = new Vector3(0f, -Mathf.Tan(cameraScreenedgeAngle * Mathf.Deg2Rad) * cameraDistance, cameraDistance);
+    }
+
+    void Update()
+    {
+        verticalMarker.position += Vector3.right * markerSpeed * Time.deltaTime;
+        horizontalMarker.position += Vector3.up * markerSpeed * Time.deltaTime;
+
+        markerText.text = verticalMarker.position.x + " X, " + horizontalMarker.position.y + " Y";
+    }
+}
