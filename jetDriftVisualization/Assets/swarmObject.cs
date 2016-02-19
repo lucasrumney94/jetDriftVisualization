4a237926bc5455a46958b4c563df07a3e72fdbd8
Hardy Whitney <htw6174@gmail.com>
2016-02-18 20:36:11 -0500

    changed swarm to look more pixel-y

--- jetDriftVisualization/Assets/swarmObject.cs
+++ jetDriftVisualization/Assets/swarmObject.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class swarmObject : MonoBehaviour {
 
+    public float zDriftMagnitude = 9f;
+
     public Vector3 targetGridSquare = new Vector3();
 
 	// Use this for initialization
@@ -19,7 +21,13 @@ public class swarmObject : MonoBehaviour {
     {
         //gameObject.GetComponent<Renderer>().material.SetFloat("_Mode", 3);
         //gameObject.GetComponent<Renderer>().material.SetFloat("_Glossiness", 0);
-        this.gameObject.GetComponent<Rigidbody>().AddForce((targetGridSquare - this.gameObject.transform.position)+9*Random.insideUnitSphere);
+        Vector3 zDrift = new Vector3(0f, 0f, Random.Range(-1f, 1f)) * zDriftMagnitude;
+        this.gameObject.GetComponent<Rigidbody>().AddForce((targetGridSquare - this.gameObject.transform.position) + zDrift); // +9*Random.insideUnitSphere);
+        //if (Vector3.Distance(targetGridSquare, transform.position) < 0.1f)
+        //{
+        //    transform.position = new Vector3(targetGridSquare.x, targetGridSquare.y, transform.position.z);
+        //    GetComponent<Rigidbody>().velocity = Vector3.zero;
+        //}
 
         this.gameObject.GetComponent<Rigidbody>().AddForce(5*new Vector3(0, 0, Mathf.Sin(Time.time + targetGridSquare.magnitude / 10*Mathf.Sin(Time.time/10))));
     }
