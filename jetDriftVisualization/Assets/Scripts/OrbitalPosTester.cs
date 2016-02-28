524a21c351155d34f23782af93efc37c8ab9319a
Hardy Whitney <htw6174@gmail.com>
2016-02-28 16:57:51 -0500

    Beginning of orbital coordinate conversion

---
+++ jetDriftVisualization/Assets/Scripts/OrbitalPosTester.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitalPosTester : MonoBehaviour {
+
+    public float inclination;
+    public float rightAscensionOfAscendingNode;
+    public float eccentricity;
+    public float arguementOfPeriapsis;
+    public float meanAnomaly;
+    public float meanMotion;
+
+    public int accuracy;
+
+    void Start()
+    {
+        Vector3 orbitalPositionVector = TLEtoVec3.Convert(inclination, rightAscensionOfAscendingNode, eccentricity, arguementOfPeriapsis, meanAnomaly, meanMotion, accuracy);
+        Debug.Log(orbitalPositionVector);
+    }
+}
---
+++ jetDriftVisualization/Assets/Scripts/Static/TLEtoVec3.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TLEtoVec3 {
+
+    public static Vector3 Convert(float inclination, float rightAscensionOfAscendingNode, float eccentricity, float arguementOfPeriapsis, float meanAnomaly, float meanMotion, int accuracy)
+    {
+        Vector3 position = Vector3.zero;
+
+
+
+        return position;
+    }
+
+    private static float EccentricAnomaly(float eccentricity, float meanAnomaly, int accuracy)
+    {
+        float eccentricAnomaly = meanAnomaly;
+        for(int i = 0; i < accuracy; i++)
+        {
+            eccentricAnomaly = meanAnomaly - (eccentricity * Mathf.Sin(eccentricAnomaly));
+        }
+
+        return eccentricAnomaly;
+    }
+}
