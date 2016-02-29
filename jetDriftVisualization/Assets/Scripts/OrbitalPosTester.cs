d7c8af8a485563b561bfc86162ec3192f27fb882
Hardy Whitney <htw6174@gmail.com>
2016-02-28 20:23:38 -0500

    eliptical coordinate conversion done

--- jetDriftVisualization/Assets/Scripts/OrbitalPosTester.cs
+++ jetDriftVisualization/Assets/Scripts/OrbitalPosTester.cs
@@ -3,18 +3,27 @@ using System.Collections;
 
 public class OrbitalPosTester : MonoBehaviour {
 
+    public float inversePositionScale;
+
     public float inclination;
     public float rightAscensionOfAscendingNode;
+    [Range(0f, 1f)]
     public float eccentricity;
     public float arguementOfPeriapsis;
+    [Range(0f, Mathf.PI * 2f)]
     public float meanAnomaly;
     public float meanMotion;
 
     public int accuracy;
 
-    void Start()
+    public GameObject planet;
+
+    void Update()
     {
         Vector3 orbitalPositionVector = TLEtoVec3.Convert(inclination, rightAscensionOfAscendingNode, eccentricity, arguementOfPeriapsis, meanAnomaly, meanMotion, accuracy);
-        Debug.Log(orbitalPositionVector);
+        orbitalPositionVector *= 1f / inversePositionScale;
+        planet.transform.position = orbitalPositionVector;
+        Debug.Log(orbitalPositionVector.x + ", " + orbitalPositionVector.y);
+        //Debug.Log(orbitalPositionVector.x - (eccentricity * Mathf.Sin(orbitalPositionVector.x)));
     }
 }
--- jetDriftVisualization/Assets/Scripts/Static/TLEtoVec3.cs
+++ jetDriftVisualization/Assets/Scripts/Static/TLEtoVec3.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public static class TLEtoVec3 {
 
+    public static float _SGP = 3.986004418f * Mathf.Pow(10, 14);
+
     public static Vector3 Convert(float inclination, float rightAscensionOfAscendingNode, float eccentricity, float arguementOfPeriapsis, float meanAnomaly, float meanMotion, int accuracy)
     {
-        Vector3 position = Vector3.zero;
+        Vector2 elipticalCoordinates = ElipticalCoordinates(eccentricity, meanAnomaly, meanMotion, accuracy);
 
-
+        Vector3 position = new Vector3(elipticalCoordinates.x, elipticalCoordinates.y, 0f);
 
         return position;
     }
@@ -17,9 +19,40 @@ public static class TLEtoVec3 {
         float eccentricAnomaly = meanAnomaly;
         for(int i = 0; i < accuracy; i++)
         {
-            eccentricAnomaly = meanAnomaly - (eccentricity * Mathf.Sin(eccentricAnomaly));
+            eccentricAnomaly = meanAnomaly + (eccentricity * Mathf.Sin(eccentricAnomaly));
         }
 
         return eccentricAnomaly;
+    }
+
+    private static float SemiMajorAxis(float meanMotion)
+    {
+        float orbitalPeriod = 1f / meanMotion; //in days
+        orbitalPeriod *= 60f * 60f * 24f; //in seconds
+
+        float smaCubed = (_SGP * (orbitalPeriod * orbitalPeriod)) / (4f * (Mathf.PI * Mathf.PI));
+
+        return Mathf.Pow(smaCubed, 1f / 3f);
+    }
+
+    private static float SemiMinorAxis(float eccentricity, float semiMajorAxis)
+    {
+        float semiMinorAxis = semiMajorAxis * Mathf.Pow(1f - (eccentricity * eccentricity), 1f / 2f);
+
+        return semiMinorAxis;
+    }
+
+    private static Vector2 ElipticalCoordinates(float eccentricity, float meanAnomaly, float meanMotion, int accuracy)
+    {
+        float eccentricAnomaly = EccentricAnomaly(eccentricity, meanAnomaly, accuracy);
+        float semiMajor = SemiMajorAxis(meanMotion);
+        float semiMinor = SemiMinorAxis(eccentricity, semiMajor);
+
+        float x = semiMajor * Mathf.Cos(eccentricAnomaly);
+        float y = semiMinor * Mathf.Sin(eccentricAnomaly);
+
+        Vector2 pos = new Vector2(x, y);
+
+        return pos;
     }
 }
